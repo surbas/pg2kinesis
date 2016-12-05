7fb5a269dc39412317834f9b15dca523314db501
Shon T. Urbas <shon.urbas@handshake.com>
2016-12-05 15:54:54 -0500

    Formatter with tests

--- formatter.py
+++ formatter.py
@@ -7,55 +7,71 @@ from .log import logger
 from backport_collections import namedtuple
 
 # Tuples representing changes as pulled from database
-Change = namedtuple('Change', 'xid, table, operation, pkey')
+Change = namedtuple(u'Change', u'xid, table, operation, pkey')
 
 # Final product of Formatter, a Change and the Change formatted.
-Message = namedtuple('Message', 'change, fmt_msg')
+Message = namedtuple(u'Message', u'change, fmt_msg')
 
-COL_TYPE_VALUE_TEMPLATE_PAT = r"{col_name}\[{col_type}\]:'?([\w\-]+)'?"
+COL_TYPE_VALUE_TEMPLATE_PAT = ur"{col_name}\[{col_type}\]:'?([\w\-]+)'?"
 
 class Formatter(object):
     VERSION = 0
-    TYPE = 'CDC'
-    IGNORED_CHANGES = {"BEGIN", "COMMIT"}
+    TYPE = u'CDC'
+    IGNORED_CHANGES = {u'COMMIT'}
 
     def __init__(self, primary_key_map, full_change=False, table_pat=None):
-        self.primary_key_patterns = {}
+        self._primary_key_patterns = {}
         self.table_re = re.compile(table_pat if table_pat is not None else ur'[/w/._]+')
         self.full_change = full_change
-        self.cur_xact = 0
+        self.cur_xact = ''
 
         for k, v in primary_key_map.iteritems():
             # ":" added to make later look up not need to trim trailing ":".
-            self.primary_key_patterns[k + ":"] = re.compile(
+            self._primary_key_patterns[k + ":"] = re.compile(
                 COL_TYPE_VALUE_TEMPLATE_PAT.format(col_name=v.col_name, col_type=v.col_type)
             )
 
-    def _preprocess_changes(self, change):
+    def _preprocess_change(self, change):
+        """
+        Takes a message payload and distills it into a Change tuple currently only looking for primary key.
+
+        They look like this:
+            u"table table_test: UPDATE: uuid[uuid]:'00079f3e-0479-4475-acff-4f225cc5188a' another_col[text]'bling'"
+
+        :param change: a message payload from postgres' test_decoding plugin.
+        :return: A namedtuple of type Change
+        """
+
         rec = change.split(' ', 3)
 
-        if rec[0] == "BEGIN":
+        if rec[0] == u'BEGIN':
             self.cur_xact = rec[1]
         elif rec[0] in self.IGNORED_CHANGES:
             pass
-        elif rec[0] == 'table':
+        elif rec[0] == u'table':
             table_name = rec[1][:-1]
 
             if self.table_re.search(table_name):
                 try:
-                    mat = self.primary_key_patterns[rec[1]].search(rec[3])
+                    mat = self._primary_key_patterns[rec[1]].search(rec[3])
                 except KeyError:
-                    logger.warning('Unable to locate table {0}'.format(rec[1]))
+                    self._log_and_raise(u'Unable to locate table: "{}"'.format(rec[1]))
                 else:
                     if mat:
                         pkey = mat.groups()[0]
                         return Change(xid=self.cur_xact, table=table_name, operation=rec[2][:-1], pkey=pkey)
+                    else:
+                        self._log_and_raise(u'Unable to locate primary key for table "{}"'.format(table_name))
         else:
-            logger.warning('Unknown change type: %s' % rec)
-            raise Exception('Unknown change type: %s' % rec)
+            self._log_and_raise(u'Unknown change: "{}"'.format(change))
+
+    @staticmethod
+    def _log_and_raise(msg):
+        logger.error(msg)
+        raise Exception(msg)
 
     def __call__(self, change):
-        pp_change = self._preprocess_changes(change)
+        pp_change = self._preprocess_change(change)
         if pp_change:
             return self.produce_formatted_message(pp_change)
 
---
+++ tests/test_formatter.py
@@ -0,0 +1,143 @@
+# coding=utf-8
+import json
+
+import mock
+import pytest
+
+from ..slot import PrimaryKeyMapItem
+from ..formatter import Change, CSVFormatter, CSVPayloadFormatter, Formatter, get_formatter
+
+
+def get_formatter_produce_formatted_message(cls):
+    # type: (Formatter, unicode) -> Message
+    change = Change(xid=1, table=u'public.blue', operation=u'Update', pkey=u'123456')
+    result = cls({}).produce_formatted_message(change)
+    assert result.change == change
+    return result
+
+
+def test_CSVFormatter_produce_formatted_message():
+    result = get_formatter_produce_formatted_message(CSVFormatter)
+
+    assert result.fmt_msg == u'0,CDC,1,public.blue,Update,123456'
+
+
+def test_CSVPayloadFormatter_produce_formatted_message():
+    result = get_formatter_produce_formatted_message(CSVPayloadFormatter)
+    assert result.fmt_msg.startswith(u'0,CDC,')
+    payload = result.fmt_msg.split(',', 2)[-1]
+    assert json.loads(payload) == dict(xid=1, table=u'public.blue', operation=u'Update', pkey=u'123456')
+
+
+@pytest.fixture
+def pkey_map():
+    return {'test_table': PrimaryKeyMapItem(u'test_table', u'uuid', u'uuid', 0),
+            'test_table2': PrimaryKeyMapItem(u'test_table2', u'name', u'character varying', 0)}
+
+
+@pytest.fixture(params=[CSVFormatter, CSVPayloadFormatter, Formatter])
+def formatter(request, pkey_map):
+    return request.param(pkey_map)
+
+
+def test___init__(formatter):
+    # processes primary key map (adds colon)
+
+    patterns = formatter._primary_key_patterns
+
+    assert len(patterns) == 2
+    assert u'test_table:' in patterns, 'with colon'
+    assert u'test_table2:' in patterns, 'with colon'
+    assert u'test_table' not in patterns, 'without colon should not be in patterns'
+    assert u'test_table2' not in patterns, 'without colon should not be in patterns'
+    assert patterns[u'test_table:'].pattern == ur"uuid\[uuid\]:'?([\w\-]+)'?"
+    assert patterns[u'test_table2:'].pattern == ur"name\[character varying\]:'?([\w\-]+)'?"
+
+
+def test__preprocess_change(formatter):
+    # assert begin -> None + cur trans
+    assert formatter.cur_xact == ''
+    result = formatter._preprocess_change(u'BEGIN 100')
+    assert result is None
+    assert formatter.cur_xact == u'100'
+
+    # assert commit -> None
+    formatter.cur_xact = ''
+    result = formatter._preprocess_change(u'COMMIT')
+    assert result is None
+    assert formatter.cur_xact == ''
+
+    # error states
+    with mock.patch.object(formatter, '_log_and_raise') as mock_log_and_raise:
+        formatter._preprocess_change(u'UNKNOWN BLING')
+        assert mock_log_and_raise.called
+        mock_log_and_raise.assert_called_with(u'Unknown change: "UNKNOWN BLING"')
+
+    with mock.patch.object(formatter, '_log_and_raise') as mock_log_and_raise:
+        formatter._preprocess_change(u"table not_a_table: UPDATE: uuid[uuid]:'00079f3e-0479-4475-acff-4f225cc5188a'")
+        assert mock_log_and_raise.called
+        mock_log_and_raise.assert_called_with(u'Unable to locate table: "not_a_table:"')
+
+    with mock.patch.object(formatter, '_log_and_raise') as mock_log_and_raise:
+        formatter._preprocess_change(u"table test_table: UPDATE: not[not]:'00079f3e-0479-4475-acff-4f225cc5188a'")
+        assert mock_log_and_raise.called
+        mock_log_and_raise.assert_called_with(u'Unable to locate primary key for table "test_table"')
+
+    # assert on proper match
+    formatter.cur_xact = '1337'
+    change = formatter._preprocess_change(
+        u"table test_table: UPDATE: uuid[uuid]:'00079f3e-0479-4475-acff-4f225cc5188a'")
+
+    assert change.xid == u'1337'
+    assert change.table == u'test_table'
+    assert change.operation == u'UPDATE'
+    assert change.pkey == u'00079f3e-0479-4475-acff-4f225cc5188a'
+
+    change = formatter._preprocess_change(
+        u"table test_table2: DELETE: name[character varying]:'Bling-2'")
+
+    assert change.xid == u'1337'
+    assert change.table == u'test_table2'
+    assert change.operation == u'DELETE'
+    assert change.pkey == u'Bling-2'
+
+
+def test_log_and_raise(formatter):
+
+    with mock.patch('logging.Logger.error') as mock_log, pytest.raises(Exception) as e_info:
+        formatter._log_and_raise(u'HELP!')
+
+    assert e_info.value.message == u'HELP!'
+    mock_log.assert_called_with(u'HELP!')
+
+
+def test___call__(formatter):
+    with mock.patch.object(formatter, '_preprocess_change', return_value=None) as mock_preprocess_change, \
+            mock.patch.object(formatter, 'produce_formatted_message', return_value=None) as mock_pfm:
+        assert not mock_preprocess_change.called
+        result = formatter('fake message')
+        assert mock_preprocess_change.called
+        assert result is None
+        assert not mock_pfm.called
+
+    with mock.patch.object(formatter, '_preprocess_change', return_value='blue') as mock_preprocess_change, \
+            mock.patch.object(formatter, 'produce_formatted_message', return_value='blue msg') as mock_pfm:
+        assert not mock_preprocess_change.called
+        result = formatter('blue message')
+        assert mock_preprocess_change.called
+        assert result == 'blue msg'
+        mock_pfm.assert_called_with('blue')
+
+
+def test_get_formatter():
+    with mock.patch.object(Formatter, '__init__', return_value=None) as mocked:
+        result = get_formatter('CSVPayload', 1, 2, 3)
+        assert isinstance(result, CSVPayloadFormatter)
+        assert mocked.called
+        mocked.assert_called_with(1, 2, 3)
+
+    with mock.patch.object(Formatter, '__init__', return_value=None) as mocked:
+        result = get_formatter('CSV', 1, 2, 3)
+        assert isinstance(result, CSVFormatter)
+        assert mocked.called
+        mocked.assert_called_with(1, 2, 3)

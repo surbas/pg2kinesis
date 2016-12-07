d3448ebf94e5eec78a01285c4888cf080bf0ea92
Shon T. Urbas <shon.urbas@handshake.com>
2016-12-07 16:22:26 -0500

    Stream tests

--- stream.py
+++ stream.py
@@ -1,5 +1,6 @@
+from __future__ import unicode_literals
 from time import sleep, time
-
+import time
 import aws_kinesis_agg.aggregator
 import boto3
 
@@ -9,23 +10,23 @@ from .log import logger
 class StreamWriter(object):
     def __init__(self, stream_name, back_off_limit=60, send_window=13):
         self.stream_name = stream_name
-        self.kinesis = boto3.client('kinesis')
         self.back_off_limit = back_off_limit
+        self.last_send = 0
+
+        self._kinesis = boto3.client('kinesis')
         self._sequence_number_for_ordering = '0'
         self._record_agg = aws_kinesis_agg.aggregator.RecordAggregator()
         self._send_window = send_window
 
-        self.last_send = 0
-
         try:
-            self.kinesis.create_stream(StreamName=stream_name, ShardCount=1)
+            self._kinesis.create_stream(StreamName=stream_name, ShardCount=1)
         except ClientError as e:
             # ResourceInUseException is raised when the stream already exists
             if e.response["Error"]["Code"] != "ResourceInUseException":
                 logger.error(e)
                 raise
 
-        waiter = self.kinesis.get_waiter('stream_exists')
+        waiter = self._kinesis.get_waiter('stream_exists')
 
         # waits up to 180 seconds for stream to exist
         waiter.wait(StreamName=self.stream_name)
@@ -37,10 +38,10 @@ class StreamWriter(object):
             agg_record = self._record_agg.add_user_record(fmt_msg.change.xid, fmt_msg.fmt_msg)
 
         # agg_record will be a complete record if aggregation is full.
-        if agg_record or (self._send_window and time() - self.last_send > self._send_window):
+        if agg_record or (self._send_window and time.time() - self.last_send > self._send_window):
             agg_record = agg_record if agg_record else self._record_agg.clear_and_get()
             self._send_agg_record(agg_record)
-            self.last_send = time()
+            self.last_send = time.time()
 
         return agg_record
 
@@ -53,22 +54,24 @@ class StreamWriter(object):
                     (agg_record.get_num_user_records(), agg_record.get_size_bytes(), pk))
 
         back_off = .05
-        while True:
+        while back_off < self.back_off_limit:
             try:
-                result = self.kinesis.put_record(Data=data,
-                                                 ExplicitHashKey=ehk,
-                                                 PartitionKey=pk,
-                                                 SequenceNumberForOrdering=self._sequence_number_for_ordering,
-                                                 StreamName=self.stream_name)
+                result = self._kinesis.put_record(Data=data,
+                                                  ExplicitHashKey=ehk,
+                                                  PartitionKey=pk,
+                                                  SequenceNumberForOrdering=self._sequence_number_for_ordering,
+                                                  StreamName=self.stream_name)
 
             except ClientError as e:
                 if e.response['Error']["Code"] == 'ProvisionedThroughputExceededException':
                     back_off *= 2
                     logger.warning('Provisioned throughput exceeded: sleeping %ss' % back_off)
-                    sleep(back_off)
+                    time.sleep(back_off)
                 else:
                     logger.error(e)
                     raise
             else:
                 logger.debug('Sequence number: %s' % result['SequenceNumber'])
                 break
+        else:
+            raise Exception('ProvisionedThroughputExceededException caused a backed off too many times!')
--- tests/test_stream.py
+++ tests/test_stream.py
@@ -1,16 +1,100 @@
-import mock
+from __future__ import unicode_literals
+
+import time
+
+from freezegun import freeze_time
+from mock import patch, call, Mock
 import pytest
+import boto3
 from botocore.exceptions import ClientError
 
 from ..stream import StreamWriter
 
-def test_StreamWriter__init__exception_handling():
-    with mock.patch('boto3.client') as mock_client:
+@pytest.fixture()
+def writer():
+    with patch('aws_kinesis_agg.aggregator.RecordAggregator'), patch.object(boto3, 'client'):
+        writer = StreamWriter('blah')
+    return writer
+
+def test__init__():
+    mock_client = Mock()
+    with patch.object(boto3, 'client', return_value=mock_client):
         error_response = {'Error': {'Code': 'ResourceInUseException'}}
-        mock_client.create_stream = mock.Mock(side_effect=lambda: ClientError(error_response))
+        mock_client.create_stream = Mock(side_effect=ClientError(error_response, 'create_stream'))
 
         StreamWriter('blah')
-        assert mock_client.called, 'Sanity'
+        assert mock_client.create_stream.called
+        assert call.get_waiter('stream_exists') in mock_client.method_calls, "We handled stream existence"
+
+        error_response = {'Error': {'Code': 'Something else'}}
+        mock_client.create_stream = Mock(side_effect=ClientError(error_response, 'create_stream'))
+
+        mock_client.reset_mock()
+        with pytest.raises(ClientError):
+            StreamWriter('blah')
+            assert mock_client.create_stream.called
+            assert call.get_waiter('stream_exists') not in mock_client.method_calls, "never reached"
 
 
+def test_put_message(writer):
 
+    writer._send_agg_record = Mock()
+
+    msg = Mock()
+    msg.change.xid = 10
+    msg.fmt_msg = object()
+
+    writer.last_send = 1445444940.0 - 10      # "2015-10-21 16:28:50"
+    with freeze_time('2015-10-21 16:29:00'):  # -> 1445444940.0
+        result = writer.put_message(None)
+
+        assert result is None, 'With no message or timeout we did not force a send'
+        assert not writer._send_agg_record.called, 'we did not force a send'
+
+        writer._record_agg.add_user_record = Mock(return_value=None)
+        result = writer.put_message(msg)
+        assert result is None, 'With message, no timeout and not a full agg we do not send'
+        assert not writer._send_agg_record.called, 'we did not force a send'
+
+    with freeze_time('2015-10-21 16:29:10'):  # -> 1445444950.0
+        result = writer.put_message(None)
+        assert result is not None, 'Timeout forced a send'
+        assert writer._send_agg_record.called, 'We sent a record'
+        assert writer.last_send == 1445444950.0, 'updated window'
+
+    with freeze_time('2015-10-21 16:29:20'):  # -> 1445444960.0
+        writer._send_agg_record.reset_mock()
+        writer._record_agg.add_user_record = Mock(return_value='blue')
+        result = writer.put_message(msg)
+
+        assert result == 'blue', 'We passed in a message that forced the agg to report full'
+        assert writer._send_agg_record.called, 'We sent a record'
+        assert writer.last_send == 1445444960.0, 'updated window'
+
+
+def test__send_agg_record(writer):
+    assert writer._send_agg_record(None) is None, 'Do not do anything if agg_record is None'
+
+    agg_rec = Mock()
+    agg_rec.get_contents = Mock(return_value=(1, 2, 'datablob'))
+
+    err = ClientError({'Error': {'Code': 'ProvisionedThroughputExceededException'}}, 'put_record')
+
+    writer._kinesis.put_record = Mock(side_effect=[err, err, err, {'SequenceNumber': 12345}])
+
+    with patch.object(time, 'sleep') as mock_sleep:
+        writer._send_agg_record(agg_rec)
+        assert mock_sleep.call_count == 3, "We had to back off 3 times so we slept"
+        assert mock_sleep.call_args_list == [call(.1), call(.2), call(.4)], 'Geometric back off!'
+
+    with pytest.raises(ClientError):
+        writer._kinesis.put_record = Mock(side_effect=ClientError({'Error': {'Code': 'Something else'}},
+                                                                  'put_record'))
+        writer._send_agg_record(agg_rec)
+
+    writer.back_off_limit = .3  # Will bust on third go around
+    writer._kinesis.put_record = Mock(side_effect=[err, err, err, {'SequenceNumber': 12345}])
+    with pytest.raises(Exception) as e_info, patch.object(time, 'sleep'):
+        writer._send_agg_record(agg_rec)
+        assert e_info.value.message == 'ProvisionedThroughputExceededException caused a backed off too many times!', \
+            'We raise on too many throughput errors'
